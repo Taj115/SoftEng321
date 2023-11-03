ce0e392a51279f13c34b3c8386ccf6c792da863f
TwoKneeZ <AKZacharatos@gmail.com>
2023-11-02 23:53:58 -0400

    Comments Added

--- src/Reviewer.java
+++ src/Reviewer.java
@@ -1,15 +1,17 @@
 package src;
 
-import java.awt.FlowLayout;
-import java.awt.Font;
 import java.util.logging.Logger;
-import javax.swing.WindowConstants;
+
+import java.awt.Font;
+import java.awt.FlowLayout;
+
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.WindowConstants;
 
 // clear;javac -d . ./src/Reviewer.java; java src.Reviewer
 public class Reviewer {
@@ -27,7 +29,7 @@ public class Reviewer {
     private JLabel label2;
 
     public Reviewer() {
-        frame = new JFrame();
+        frame = new JFrame("Reviewer BETA");
 
         panel = new JPanel();
         button = new JButton("Submit");
@@ -36,29 +38,34 @@ public class Reviewer {
         label1 = new JLabel("Immigrant ID:");
         label2 = new JLabel("Immigrant Status:");
 
+        // Aligns These Elements to the Right side
         JPanel tranPanel1 = new JPanel();
         tranPanel1.setLayout(new FlowLayout(FlowLayout.RIGHT));
         tranPanel1.add(label1);
         tranPanel1.add(text1);
 
+        // Aligns These Elements to the Right Side
         JPanel tranPanel2 = new JPanel();
         tranPanel2.setLayout(new FlowLayout(FlowLayout.RIGHT));
         tranPanel2.add(label2);
         tranPanel2.add(text2);
 
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS ));
+        // Structures it so that they are on top of each other going down
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.add(tranPanel1);
         panel.add(tranPanel2);
         panel.add(button);
 
+        // Specific Button Interaction
         button.addActionListener(event -> {
-            String inText = text2.getText();
+            String inText = text2.getText(); // Grabs whats in Text Box
             logger.info("Submitted " + inText + " to Approver...");
 
             // Updates Status => if valid continue if not do nothing...
             if (!acc.sendImmigrantStatus(inText))
                 return;
 
+            // Kills current Window
             frame.dispose();
 
             frame = new JFrame();
@@ -72,12 +79,11 @@ public class Reviewer {
             frame.setVisible(true);
             frame.setResizable(false);
             frame.add(label1);
-
         });
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(350, 200);
-        frame.setLayout(new FlowLayout(FlowLayout.CENTER));
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER)); // Makes Sure items are centered with no weird gaps
         frame.setVisible(true);
         frame.setResizable(false);
         frame.add(panel);
